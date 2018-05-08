9639984503a095f9b18ef5cb41019899ec9f728e
Rajesh M <yem.rajesh@yahoo.co.in>
2018-05-08 10:14:21 -0400

    new singletonclass file

---
+++ JavaConceptsLearn/src/com/learn/javacollections/SingletonConcept.java
@@ -0,0 +1,43 @@
+package com.learn.javacollections;
+
+public class SingletonConcept {
+
+	// Singleton class is a class that can have only 1 object
+	// Design of the singleton class:
+	// 1. Make constructor as private
+	// 2. Write a static method which has return type object of this Singleton class
+	// (called as lazy initialization)
+
+	private static SingletonConcept sc = null;
+	public String str;
+
+	private SingletonConcept() {
+		str = "Test string in singleton class";
+	}
+
+	public static SingletonConcept getInstance() {
+		if (sc == null)
+			sc = new SingletonConcept();
+
+		return sc;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SingletonConcept a1 = SingletonConcept.getInstance();
+		SingletonConcept a2 = SingletonConcept.getInstance();
+		SingletonConcept a3 = SingletonConcept.getInstance();
+		
+		a1.str = a1.str.toUpperCase().concat(" - Rajesh");
+		System.out.println("A1 str: " +a1.str);
+		System.out.println("A2 str: " +a2.str);
+		System.out.println("A3 str: " +a3.str);
+		
+		a3.str = "replaced from a3";
+		System.out.println("A1 str: " +a1.str);
+		System.out.println("A2 str: " +a2.str);
+		System.out.println("A3 str: " +a3.str);
+	}
+
+}
